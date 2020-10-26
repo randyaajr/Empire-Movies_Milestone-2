17607947f044f265d84ff1852d07080947e68bfe
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-10-26 15:16:31 -0400

    getMovie function now fetches film data and displays on movie.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,14 +50,47 @@ function movieSelected(id) {
 
 //This fuction takes the movie from sessionStorage and display the information on the screen.
 function getMovie() {
-    let movieID = sessionStorage.getItem('movieID');
+    let movieId = sessionStorage.getItem('movieId');
 
 
-    axios.get('http://www.omdbapi.com?i=' + movieID)
-        .then((response) => {
-            console.log(response);
+    // Makes a request for th user, with the id of the selected movie
+    axios.get("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=98325a9d3ed3ec225e41ccc4d360c817") //New key needed
+        .then(function(response) {
+            let movie = response.data;
+            //console.log(movie); (For debugging reasons)
+
+            //Output section which will populate movie.html will requested film data
+            let output = `
+          <div class="row">
+            <div class="col-md-4">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="thumbnail">
+            </div>
+            <div class="col-md-8">
+              <h2>${movie.title}</h2>
+              <ul class="list-group">
+                <li class="list-group-item"><strong>Genre:</strong> ${movie.genres[0].name}, ${movie.genres[1].name}</li>
+                <li class="list-group-item"><strong>Released:</strong> ${movie.release_date}</li>
+                <li class="list-group-item"><strong>Rated:</strong> ${movie.vote_average}</li>
+                <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime} min.</li>
+                <li class="list-group-item"><strong>Production Companies:</strong> ${movie.production_companies[0].name} min.</li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="well">
+              <h3>Plot</h3>
+              ${movie.overview}
+              <hr>
+              <a href="http://imdb.com/title/${movie.imdb_id}" target="_blank" class="btn btn-primary">View IMDB</a>
+              <a href="index.html" class="btn btn-default">Go Back To Search</a>
+            </div>
+          </div>
+      `;
+            $('#movie').html(output);
         })
-        .catch(function(err) {
-            console.log(err);
-        });
+
+
+    .catch(function(error) {
+        console.log(error);
+    });
 }
