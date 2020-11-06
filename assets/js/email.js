040e9f309a18d872641f233354e4be4a863127ce
randyaajr <67880000+randyaajr@users.noreply.github.com>
2020-11-05 20:31:05 -0500

    Emailjs app completed

---
+++ assets/js/email.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "randy", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.moviesummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+
+    return false;
+}
